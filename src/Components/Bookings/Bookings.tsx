8aa5a60ad6abbc8234cf6644f384e7a21fcdea12
123Somrat <jafaruddin875@gmail.com>
2024-04-09 22:42:40 +0200

    set selected date on state

--- src/Components/Bookings/Bookings.tsx
+++ src/Components/Bookings/Bookings.tsx
@@ -1,13 +1,19 @@
+import { useState } from "react";
 import { Calendar } from "react-calendar";
 import "react-calendar/dist/Calendar.css";
 export default function Bookings() {
 
-
+    // hold user selected date
+    const [selectedDate, setSelectedDate] = useState<Date>();
+    
     // create handleDateClick function for getting user clicked date
     const handleDateClick = (date: Date) => {
-         console.log(date)
+         // set date on selected date state
+         setSelectedDate(date)
       };
+
       
+
   return (
     <div className="flex justify-center ">
       <Calendar 
