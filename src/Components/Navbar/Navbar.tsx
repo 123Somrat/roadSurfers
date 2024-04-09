e55391612584e883efffc33359cff5592a3f0e7a
123Somrat <jafaruddin875@gmail.com>
2024-04-09 14:06:23 +0200

    Passing station as a props

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -1,19 +1,23 @@
+
 import useFetchData from "../../hooks/useFetchData";
 import Logo from "../Logo/Logo";
 import StationDropDown from "../StationDropDown/StationDropDown";
 
 
 export default function Navbar() {
-    
+
      // Fetching all stations
     const stations = useFetchData("/stations");
+   
 
+
+ 
 
 
   return (
     <div className="flex justify-between p-2 bg-[#7bdcb5]">
        <Logo/>
-       <StationDropDown />
+       <StationDropDown stations={stations} />
     </div>
   )
 }
--- src/Components/StationDropDown/StationDropDown.tsx
+++ src/Components/StationDropDown/StationDropDown.tsx
@@ -1,6 +1,28 @@
 
+interface Booking {
+  id: string;
+  pickupReturnStationId: string;
+  customerName: string;
+  startDate: string;
+  endDate: string;
+}
 
-export default function StationDropDown() {
+// define stations data types
+interface Station {
+  id: string;
+  name: string;
+  bookings: Booking[];
+}
+
+// Define Props data types
+interface StationDropDownProps {
+  stations?: Station;
+ 
+
+}
+
+export default function StationDropDown({stations}:StationDropDownProps) {
+  console.log(stations)
   return (
     <div>
         <h1>Choose Station</h1>
