3cd7a8f8ad8e08b9a0d8da7e8e45e099df44148d
123Somrat <jafaruddin875@gmail.com>
2024-04-10 00:01:36 +0200

    Showing the bookings in respective date and showing all booking in a modal

--- src/Components/Bookings/Bookings.tsx
+++ src/Components/Bookings/Bookings.tsx
@@ -1,6 +1,9 @@
 import { useState } from "react";
 import { Calendar } from "react-calendar";
 import "react-calendar/dist/Calendar.css";
+import { Button, Modal } from "flowbite-react";
+import { format } from 'date-fns';
+import { Link } from "react-router-dom";
 
 
 interface Booking {
@@ -21,17 +24,23 @@ interface Booking {
   // Define Props data types
   interface StationDropDownProps {
     stations?: Station[];
-    station: string;
+    selectedStation: string;
   }
 
 
-export default function Bookings({stations , station}:StationDropDownProps) {
+export default function Bookings({stations , selectedStation}:StationDropDownProps) {
 
     // hold user selected date
     const [selectedDate, setSelectedDate] = useState<Date>();
 
     //hold Current week
     const [currentWeek, setCurrentWeek] = useState<Date>(new Date("2020-05-01T15:43:54.055Z"));
+
+    // Toggle Modal
+    const [openModal, setOpenModal] = useState(false);
+
+   // find the selected station data from Stations
+   const choosenStationBookings = stations?.find(station=>station.name===selectedStation)
 
     // create handleDateClick function for getting user clicked date
     const handleDateClick = (date: Date) => {
@@ -43,10 +52,22 @@ export default function Bookings({stations , station}:StationDropDownProps) {
       const handleWeekChange = (date:any) =>{
          // set the current week in state
           setCurrentWeek(date)
+         // change the state value for open modal
+          setOpenModal(true)
         
       };
       
-    console.log(selectedDate,stations,station)
+      // Format the booking startdate and then retun the formatted date array
+      const getBookingsForDate = (date:Date) => {
+        const formattedDate = format(date, 'yyyy-MM-dd');
+        return choosenStationBookings?.bookings.filter((booking) => format(booking.startDate, 'yyyy-MM-dd' ) === formattedDate);
+      };
+
+      const renderTileContent = ({ date }) => {
+        const bookingsForDate = getBookingsForDate(date);
+        return bookingsForDate?.length > 0 ? <div className="text-emerald-500">Booked</div> : null;
+      };
+
 
 
 
@@ -59,9 +80,28 @@ export default function Bookings({stations , station}:StationDropDownProps) {
       value={currentWeek}
       onClickDay={handleDateClick}
       onChange={handleWeekChange}
+      tileContent={renderTileContent}
       />
 
-
+{/*Showing bookings in MOdal respective on Date */}
+<Modal show={openModal} onClose={() => setOpenModal(false)} className="md:h-[500px] lg:h-full">
+        <Modal.Header>Bookings for {selectedDate && format(selectedDate, 'dd/MM/yyyy')}</Modal.Header>
+        <Modal.Body>
+          <div className="space-y-6">
+          {selectedDate &&
+              getBookingsForDate(selectedDate)?.map((booking,index) => (
+               <div className="flex justify-between" key={booking.id}>
+                   <li key={booking.id} className="list-none"><span className="font-bold">{index+1} .  </span>  {booking.customerName}</li>
+                  <Link to={`/stations/${choosenStationBookings?.id}/bookings/${booking.id}`}><button className="text-white bg-emerald-400 hover:bg-emerald-500 p-[3px] w-16 rounded-sm">Details</button></Link> 
+               </div>   
+            ))}
+          </div>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button className="bg-emerald-500 text-white " onClick={() => setOpenModal(false)}>Close</Button>
+        
+        </Modal.Footer>
+      </Modal>
      
  
 
