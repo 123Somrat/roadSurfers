de4fbf6c40e09ac363433463755ae4e18ac8a27c
123Somrat <jafaruddin875@gmail.com>
2024-04-09 13:36:21 +0200

    Write useEffect cleanup function

--- src/hooks/useFetchData.tsx
+++ src/hooks/useFetchData.tsx
@@ -1,56 +1,47 @@
 import { useEffect, useState } from "react";
 import api from "../api/api";
 
-
-
 // define Booking data types
 interface Booking {
-    id: string;
-    pickupReturnStationId: string;
-    customerName: string;
-    startDate: string;
-    endDate: string;
-  }
-  
-  // define Api response data types
-  interface Station {
-    id: string;
-    name: string;
-    bookings: Booking[];
-  }
+  id: string;
+  pickupReturnStationId: string;
+  customerName: string;
+  startDate: string;
+  endDate: string;
+}
 
+// define Api response data types
+interface Station {
+  id: string;
+  name: string;
+  bookings: Booking[];
+}
 
+export default function useFetchData(url: string) {
+  // for hold fetched data which is we got from api response
+  const [data, setData] = useState<Station>();
 
-
-export default function useFetchData(url:string) {
-     
-    // for hold fetched data which is we got from api response
-     const [data , setData ] = useState<Station>()
-     
-    //  useEffect for  side Effect like data fetching
-    useEffect(()=>{
-         
+  //  useEffect for  side Effect like data fetching
+  useEffect(() => {
     // create async Data Fetcher function
-     const fetchingData =async ()=>{
-         try{   
-            const response = await api.get(url)
-            const data = await response.data
-            setData(data)
-         }catch(error){
-             
-            console.log(error)
+    const fetchingData = async () => {
+      try {
+        const response = await api.get(url);
+        const data = await response.data;
+        setData(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
 
-         }
-        
+    // call data fetcher function
+    fetchingData();
 
+    // useEffect cleanUp function
+    return () => {
+      console.log("Cleanup: Component unmounted.");
+    };
+  }, []);
 
-
-     }
-
-      // call data fetcher function
-      fetchingData()
-
-    },[])
-
-    return data
+  return data;
 }
