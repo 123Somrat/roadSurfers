626d807832ba63b0b50dff595859d4142d3ed584
123Somrat <jafaruddin875@gmail.com>
2024-04-09 13:24:07 +0200

    Fetching data depends on given url

--- src/hooks/useFetchData.tsx
+++ src/hooks/useFetchData.tsx
@@ -10,10 +10,12 @@ export default function useFetchData(url:string) {
     // create async Data Fetcher function
      const fetchingData =async ()=>{
          try{   
-             
+            const response = await api.get(url)
+            const data = await response.data
 
          }catch(error){
-
+             
+            console.log(error)
 
          }
         
