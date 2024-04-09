7ed7e47d49275a6259ed1a6023dde1a438daaf89
123Somrat <jafaruddin875@gmail.com>
2024-04-09 12:51:16 +0200

    Call Navbar component in Layout component

--- src/LayOuts/Layouts.tsx
+++ src/LayOuts/Layouts.tsx
@@ -1,12 +1,11 @@
 import { Outlet } from "react-router-dom";
+import Navbar from "../Components/Navbar/Navbar";
 
 export default function Layouts() {
   return (
     <div className="container max-w-6xl mx-auto min-h-screen border-2 border-slate-500">
       <nav>
-        <h1 className="text-red-400">
-          This is navBar whis is always exziest in all page
-        </h1>
+          <Navbar/>
       </nav>
 
       <div>
