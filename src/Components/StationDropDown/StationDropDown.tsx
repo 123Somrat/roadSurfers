975df5a937f56e09156781b1d68af9234638bb4b
123Somrat <jafaruddin875@gmail.com>
2024-04-09 14:28:15 +0200

    Create handler for collect Chosen station name

--- src/Components/StationDropDown/StationDropDown.tsx
+++ src/Components/StationDropDown/StationDropDown.tsx
@@ -21,12 +21,23 @@ interface StationDropDownProps {
 }
 
 export default function StationDropDown({ stations }: StationDropDownProps) {
+
+  // create handleer function for collect selected station name
+  const handleStation = (name:string)=>{
+     console.log(name)
+}
+
+
+
   return (
     <div>
       <Dropdown label="Choose station" color="white">
         {stations &&
           stations?.map((station) => (
-            <Dropdown.Item key={station.id}>{station.name}</Dropdown.Item>
+            <Dropdown.Item 
+            key={station.id}
+            onClick={()=>handleStation(station.name)}
+            >{station.name}</Dropdown.Item>
           ))}
       </Dropdown>
     </div>
