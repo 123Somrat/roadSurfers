dd588f583bc3d70a439ea97d3ff227f0c1af612b
123Somrat <jafaruddin875@gmail.com>
2024-04-09 22:38:31 +0200

    create handleDateClick function for getting user selected date

--- src/Components/Bookings/Bookings.tsx
+++ src/Components/Bookings/Bookings.tsx
@@ -1,9 +1,21 @@
 import { Calendar } from "react-calendar";
 import "react-calendar/dist/Calendar.css";
 export default function Bookings() {
+
+
+    // create handleDateClick function for getting user clicked date
+    const handleDateClick = (date: Date) => {
+         console.log(date)
+      };
+      
   return (
     <div className="flex justify-center ">
-      <Calendar nextLabel="next" prevLabel="prev" showWeekNumbers />
+      <Calendar 
+      nextLabel="next" 
+      prevLabel="prev" 
+      showWeekNumbers 
+      onClickDay={handleDateClick}
+      />
     </div>
   );
 }
