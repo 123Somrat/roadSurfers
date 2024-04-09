0c7c486719f1afcff7da2300e307a96ce717f563
123Somrat <jafaruddin875@gmail.com>
2024-04-09 13:22:39 +0200

    Create async data Fetcher function and call it

--- src/hooks/useFetchData.tsx
+++ src/hooks/useFetchData.tsx
@@ -1,7 +1,29 @@
+import { useEffect } from "react";
+import api from "../api/api";
 
 
-export default function useFetchData() {
-  return (
-    <div>useFetchData</div>
-  )
+export default function useFetchData(url:string) {
+
+    //  useEffect for  side Effect like data fetching
+    useEffect(()=>{
+         
+    // create async Data Fetcher function
+     const fetchingData =async ()=>{
+         try{   
+             
+
+         }catch(error){
+
+
+         }
+        
+
+
+
+     }
+
+      // call data fetcher function
+      fetchingData()
+
+    },[])
 }
