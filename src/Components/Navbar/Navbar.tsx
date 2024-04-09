54bf59666713906cda7db5dda57fd777504906bf
123Somrat <jafaruddin875@gmail.com>
2024-04-09 12:49:01 +0200

    Create Logo StationDropDown component and call It In Navbar

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -1,7 +1,12 @@
-import React from 'react'
+import Logo from "../Logo/Logo";
+import StationDropDown from "../StationDropDown/StationDropDown";
+
 
 export default function Navbar() {
   return (
-    <div>Navbar</div>
+    <div className="flex justify-between p-2 bg-[#7bdcb5]">
+       <Logo/>
+       <StationDropDown/>
+    </div>
   )
 }
---
+++ src/Components/StationDropDown/StationDropDown.tsx
@@ -0,0 +1,9 @@
+
+
+export default function StationDropDown() {
+  return (
+    <div>
+        <h1>Choose Station</h1>
+    </div>
+  )
+}
