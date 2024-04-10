13f4ff96d9549c73d13ffccb96086c37da81e128
123Somrat <jafaruddin875@gmail.com>
2024-04-10 21:38:46 +0200

    import useParams hooks and destructure dynamic params from params objject

--- src/Pages/BookingDetails/BookingDetails.tsx
+++ src/Pages/BookingDetails/BookingDetails.tsx
@@ -1,7 +1,13 @@
-
+import { useParams } from "react-router-dom";
 
 export default function BookingDetails() {
+   // Useing useParam hooks for getting dynamic params from url
+   // destructure stationId and booking_id from Params object
+   const { stationId , booking_id} =  useParams(); 
+
   return (
-    <div>BookingDetails</div>
+    <div>
+         <h1>Hi i am booking Deatils page</h1>
+    </div>
   )
 }
