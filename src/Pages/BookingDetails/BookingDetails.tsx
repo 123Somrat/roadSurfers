c2265b982367559fa75ede03f1b05182d6550ce4
123Somrat <jafaruddin875@gmail.com>
2024-04-10 22:55:39 +0200

    Showing booking Deatis

--- src/Pages/BookingDetails/BookingDetails.tsx
+++ src/Pages/BookingDetails/BookingDetails.tsx
@@ -19,15 +19,34 @@ export default function BookingDetails() {
     // getting endDate from booking
     const endDate = booking?.endDate
     // calculate total trip duration depends on startDate and endDate
-    const duration = differenceInDays(endDate,startdate)
+    const bookingDuration = differenceInDays(endDate,startdate)
     
     // formatting the startDate
     const bookingStartDate = format(new Date(startdate ? startdate : '2020-05-06T17:02:22.932Z'), "yyyy-MM-dd");
     
    
   return (
-    <div>
-         <h1>Hi i am booking Deatils page</h1>
-    </div>
+    <div className='w-full lg:p-2 '>
+    <div className='w-3/3 md:w-2/3 h-64 mx-auto p-4 flex justify-around border-2 border-green-300 '>
+     <div >
+           <h1 className=" font-medium p-2 ">GuestName : </h1>
+       
+           <h1 className="font-medium p-2">Station : </h1>
+           
+           <h1 className="font-medium p-2">BookingDate : </h1>
+           
+           <h1 className="font-medium p-2 mb-2">BookingDuration : </h1>
+            <button className=" md:ml-[6px] bg-emerald-400 hover:bg-emerald-500 p-2 rounded-sm text-white">back to Home</button>
+        </div>
+          <div>
+          <p className="p-2">{customerName}</p>
+          <p className="p-2">'berlin</p>
+          <p className="p-2">{bookingStartDate}</p>
+          <p className="p-2">{bookingDuration} Days</p>
+          </div>
+          
+        </div>
+        
+  </div>
   )
 }
